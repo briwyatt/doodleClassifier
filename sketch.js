ea74ef78dded37ec3a5abfaaab236fddf5421ec6
Bri Wyatt <mbriwyatt@gmail.com>
2018-03-13 20:30:18 -0400

    all data files are loaded into project and created function to separate training and testing sets

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 // console.log("inside doodle Classifier");
 
-let cats;
-let trains;
-let rainbows;
+const len = 784;
+const total_data = 1000;
+
+let cats_data;
+let trains_data;
+let rainbows_data;
+
+let cats = {};
+let trains = {};
+let rainbows = {};
+
 
 
 // p5 version 0.65 (?)
@@ -11,16 +19,36 @@ let rainbows;
 // Uint8Array(784000) type of array that can only store integers
 
 
+// divide data into training and testing sets
+function prepareData(category, data){
+    category.training = [];
+    category.testing = [];
+    for (let i = 0; i < total_data; i++) {
+        let offset = i * len;
+        let threshold = floor(0.8 * total_data);
+        if(i < threshold){
+            category.training[i] = data.bytes.subarray(offset, offset + len);
+        } else {
+            category.testing[i - threshold] = data.bytes.subarray(offset, offset + len);
+        }
+    }
+}
+
+
+
 function preload(){
-    cats = loadBytes('data/cats1000.bin');
-    trains = loadBytes('data/trains1000.bin');
-    rainbows = loadBytes('data/rainbows1000.bin');
+    cats_data = loadBytes('data/cats1000.bin');
+    trains_data = loadBytes('data/trains1000.bin');
+    rainbows_data = loadBytes('data/rainbows1000.bin');
 }
 
 
 function setup() {
     createCanvas(280, 280);
     background(0);
+    prepareData(cats, cats_data);
+    prepareData(trains, trains_data);
+    prepareData(rainbows, rainbows_data);
 
     let total = 100;
     // each image
@@ -30,9 +58,9 @@ function setup() {
         let offset = n * 784;
         // each pixel of each image
         for(let i = 0; i < 784 ; i ++){
-             // whats that single value inside the cats.bytes array: 
+             // look at that single value inside the cats.bytes array: 
                  // I need to take that value and give it to the red, green, & blue parts
-            let val = 255 - cats.bytes[i + offset];
+            let val = 255 - cats_data.bytes[i + offset];
             img.pixels[i * 4 + 0] = val; 
             img.pixels[i * 4 + 1] = val; 
             img.pixels[i * 4 + 2] = val;
